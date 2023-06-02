753e2b08d7cc303ceb95669ff6709d7dcbdb8565
maxmmin <rese71196@gmail.com>
2023-06-02 10:21:06 +0300

    exploration modes view component

--- src/main/react/screens/Explore/ExplorationModesView.tsx
+++ src/main/react/screens/Explore/ExplorationModesView.tsx
@@ -2,8 +2,7 @@ import {Entity, ExplorationMode} from "../../../redux/exploration/EntityExplorat
 import {Form} from "react-bootstrap";
 import React, {ChangeEvent, useEffect} from "react";
 import {useAppDispatch, useAppSelector} from "../../../redux/hooks";
-import store from "../../../redux/store";
-import {PersonExplorationParams, PersonExplorationState} from "../../../redux/exploration/PersonExploration";
+import {PersonExplorationState} from "../../../redux/exploration/PersonExploration";
 import {JurPersonExplorationState} from "../../../redux/exploration/JurPersonExploration";
 import {UserExplorationState} from "../../../redux/exploration/UserExploration";
 import ExplorationManager from "../../../redux/exploration/ExplorationStateManager";
@@ -41,24 +40,24 @@ const ExplorationModesView = () => {
     const explorationModes: ExplorationMode[]|null = explorationParams?explorationParams.supportedModes:null;
 
     function handleSelectChange(event: ChangeEvent<HTMLSelectElement>): void {
+        const modeIndex = Number(event.currentTarget.value);
 
-        const mode = (ExplorationMode as any)[event.currentTarget.value]
+        const mode = explorationModes![modeIndex];
 
         if (mode) {
             explorationManager?.switchExplorationMode(mode);
             return;
-        }
-        throw new Error("unknown mode");
+        } else  throw new Error("unknown mode");
     }
 
-    if (modes&&explorationMode) {
+    if (explorationMode&&explorationModes) {
         return (
             <>
                 <p style={{marginBottom: '10px'}}>Пошук за:</p>
 
-                <Form.Select className="explore__select" value={explorationMode} onChange={handleSelectChange}>
-                    {modes.map(([key, {title}], index)=>
-                        <option key={index} value={key}>{title}</option>)
+                <Form.Select className="explore__select" value={explorationModes.indexOf(explorationMode)} onChange={handleSelectChange}>
+                    {explorationModes.map((mode, index)=>
+                        <option key={index} value={index}>{mode.title}</option>)
                     }
                 </Form.Select>
             </>
