51e412a9e17b9cb261db709d833d2292e3e07bb2
maxmmin <rese71196@gmail.com>
2023-06-30 18:47:17 +0300

    validation service inject

--- src/main/service/creation/CreationServiceImpl.ts
+++ src/main/service/creation/CreationServiceImpl.ts
@@ -8,6 +8,9 @@ import handleCreationError from "./handleCreationError";
 import DtoMapper from "../../rest/dto/dtoMappers/DtoMapper";
 import CreationApiService from "./api/CreationApiService";
 import CreationStateManager from "./stateManager/CreationStateManager";
+import ValidationService from "../ValidationService";
+import {hasErrors} from "../exploration/validation/BasicExplorationValidationService";
+import ValidationError from "../../error/ValidationError";
 
 
 /**
@@ -21,13 +24,16 @@ class CreationServiceImpl<RequestDto,E,ResponseDto> implements CreationService {
     private mapper: DtoMapper<RequestDto, E, ResponseDto>;
     private apiService: CreationApiService<RequestDto, ResponseDto>;
     private creationStateManager: CreationStateManager<E>;
+    private validationService: ValidationService<E>
 
     constructor(apiService: CreationApiService<RequestDto, ResponseDto>,
                 creationStateManager: CreationStateManager<E>,
-                mapper: DtoMapper<RequestDto, E, ResponseDto>) {
+                mapper: DtoMapper<RequestDto, E, ResponseDto>,
+                validationService: ValidationService<E>) {
         this.mapper = mapper;
         this.apiService = apiService;
         this.creationStateManager = creationStateManager;
+        this.validationService = validationService;
     }
 
     createEntity(): void {
@@ -39,11 +45,18 @@ class CreationServiceImpl<RequestDto,E,ResponseDto> implements CreationService {
         ThunkArg<{emergingEntity: E}>,
         LitmusAsyncThunkConfig>(CreationTypedAction.person[CreationCoreAction.CREATE_ENTITY],async ({emergingEntity}, {rejectWithValue, fulfillWithValue}) => {
         try {
+            const errors = this.validationService.validate(emergingEntity);
+            if (hasErrors(errors)) {
+                throw new ValidationError(errors);
+            }
             const requestDto: RequestDto = this.mapper.mapToRequestDto(emergingEntity);
             const responseDto: ResponseDto = await this.apiService.create(requestDto);
             const entity: E = this.mapper.mapToEntity(responseDto);
             return fulfillWithValue(deepCopy(entity), {notify: true});
         } catch (e: unknown) {
+            if (e instanceof ValidationError) {
+                this.creationStateManager.setValidationErrors(e.errors);
+            }
             return rejectWithValue(handleCreationError(e), {notify: true});
         }
     })
