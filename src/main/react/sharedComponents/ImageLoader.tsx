fadb7420220d5b1028e68f7e4eb334ccc61a11c3
maxmmin <rese71196@gmail.com>
2023-07-28 15:47:56 +0300

    backup

---
+++ src/main/react/sharedComponents/ImageLoader.tsx
@@ -0,0 +1,9 @@
+type ImageLoaderProps = {
+    containerCssPrefix?: string
+}
+
+export default function ImageLoader () {
+    return (
+        <div className={`image-loader-container`}></div>
+    )
+}
