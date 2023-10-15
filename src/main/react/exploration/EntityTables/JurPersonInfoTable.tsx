3907b4a82417d2cfa57692dcd1b5a5fcfcd858e2
secret <x@gmail.com>
2023-10-15 21:41:45 +0300

    some stylings

--- src/main/react/exploration/EntityTables/JurPersonInfoTable.tsx
+++ src/main/react/exploration/EntityTables/JurPersonInfoTable.tsx
@@ -8,24 +8,26 @@ type Props = {
 
 const JurPersonInfoTable = ({jurPerson}: Props) => {
     return (
-        <div className={"entity-container jur-person-container"}>
-            <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">ID</h6></div>
-            <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">Назва</h6></div>
-            <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">ЄДРПОУ</h6></div>
-            <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">Дата реєстрації</h6></div>
-            <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">Власник</h6></div>
-            <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">Бенефіціарний власник</h6></div>
-            <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">Адреса</h6></div>
+        <div className="entity-container-wrapper jur-person-container-wrapper">
+                <div className={"entity-container jur-person-container"}>
+                        <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">ID</h6></div>
+                        <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">Назва</h6></div>
+                        <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">ЄДРПОУ</h6></div>
+                        <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">Дата реєстрації</h6></div>
+                        <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">Власник</h6></div>
+                        <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">Бенефіціарний власник</h6></div>
+                        <div className="entity-container__column-title entity-container__column-title-block_jur-person"><h6 className="entity-container__column-title entity-container__column-title_jur-person">Адреса</h6></div>
 
-            <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.id?jurPerson.id:noInfoMessage}</p></div>
-            <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.name?jurPerson.name:noInfoMessage}</p></div>
-            <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.edrpou?jurPerson.edrpou:noInfoMessage}</p></div>
-            <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.dateOfRegistration?DateEntityTool.getStringFrom(jurPerson.dateOfRegistration):noInfoMessage}</p></div>
-            <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.owner?jurPerson.owner.lastName:noInfoMessage}</p></div>
-            <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.benOwner?jurPerson.benOwner.lastName:noInfoMessage}</p></div>
-            <div className="entity-container__value entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.location?jurPerson.location.address:noInfoMessage}</p></div>
+                        <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.id?jurPerson.id:noInfoMessage}</p></div>
+                        <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.name?jurPerson.name:noInfoMessage}</p></div>
+                        <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.edrpou?jurPerson.edrpou:noInfoMessage}</p></div>
+                        <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.dateOfRegistration?DateEntityTool.getStringFrom(jurPerson.dateOfRegistration):noInfoMessage}</p></div>
+                        <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.owner?jurPerson.owner.lastName:noInfoMessage}</p></div>
+                        <div className="entity-container__value-block entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.benOwner?jurPerson.benOwner.lastName:noInfoMessage}</p></div>
+                        <div className="entity-container__value entity-container__value-block_jur-person"><p className="entity-container__value entity-container__value_jur-person">{jurPerson.location?jurPerson.location.address:noInfoMessage}</p></div>
+                </div>
         </div>
-)
+        )
 }
 
 export default JurPersonInfoTable
--- src/main/react/exploration/EntityTables/PersonInfoTable.tsx
+++ src/main/react/exploration/EntityTables/PersonInfoTable.tsx
@@ -15,30 +15,32 @@ const PersonInfoTable = ({person}: Props) => {
     const passportData = person.passportData;
 
     return (
-    <div className={"entity-container person-container"}>
-        <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">ID</h6></div>
-        <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Прізвище</h6></div>
-        <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Ім'я</h6></div>
-        <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Ім'я по-батькові</h6></div>
-        <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Дата народження</h6></div>
-        <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Номер паспорта</h6></div>
-        <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Серія паспорта</h6></div>
-        <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">РНОКПП</h6></div>
-        <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Адреса</h6></div>
+    <div className="entity-container-wrapper person-container-wrapper">
+        <div className={"entity-container person-container"}>
+            <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">ID</h6></div>
+            <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Прізвище</h6></div>
+            <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Ім'я</h6></div>
+            <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Ім'я по-батькові</h6></div>
+            <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Дата народження</h6></div>
+            <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Номер паспорта</h6></div>
+            <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Серія паспорта</h6></div>
+            <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">РНОКПП</h6></div>
+            <div className="entity-container__column-title-block entity-container__column-title-block_person"><h6 className="entity-container__column-title entity-container__column-title_person">Адреса</h6></div>
 
-        <div className="entity-container__value entity-container__value-block_person">
-            <p className="entity-container__value entity-container__value_person">
-                <NavLink to={buildUrl(appConfig.applicationMappings.entityRoot[Entity.PERSON], person.id.toString())}>{person.id}</NavLink>
-            </p>
+            <div className="entity-container__value entity-container__value-block_person">
+                <p className="entity-container__value entity-container__value_person">
+                    <NavLink to={buildUrl(appConfig.applicationMappings.entityRoot[Entity.PERSON], person.id.toString())}>{person.id}</NavLink>
+                </p>
+            </div>
+            <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{person.lastName}</p></div>
+            <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{person.firstName}</p></div>
+            <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{person.middleName?person.middleName:noInfoMessage}</p></div>
+            <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{person.dateOfBirth?DateEntityTool.getStringFrom(person.dateOfBirth):noInfoMessage}</p></div>
+            <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{passportData?.passportNumber?passportData.passportNumber:noInfoMessage}</p></div>
+            <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{passportData?.passportSerial?passportData.passportSerial:noInfoMessage}</p></div>
+            <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{passportData?.rnokppCode?passportData.rnokppCode:noInfoMessage}</p></div>
+            <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{person.location?person.location.address:noInfoMessage}</p></div>
         </div>
-        <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{person.lastName}</p></div>
-        <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{person.firstName}</p></div>
-        <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{person.middleName?person.middleName:noInfoMessage}</p></div>
-        <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{person.dateOfBirth?DateEntityTool.getStringFrom(person.dateOfBirth):noInfoMessage}</p></div>
-        <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{passportData?.passportNumber?passportData.passportNumber:noInfoMessage}</p></div>
-        <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{passportData?.passportSerial?passportData.passportSerial:noInfoMessage}</p></div>
-        <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{passportData?.rnokppCode?passportData.rnokppCode:noInfoMessage}</p></div>
-        <div className="entity-container__value entity-container__value-block_person"><p className="entity-container__value entity-container__value_person">{person.location?person.location.address:noInfoMessage}</p></div>
     </div>
     )
 }
