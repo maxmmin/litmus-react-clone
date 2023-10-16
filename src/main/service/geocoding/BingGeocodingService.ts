3034cb1a7e48d761fe2e72ccb433308ab1c50275
secret <x@gmail.com>
2023-10-16 12:46:11 +0300

    bing geocoding service fix

--- src/main/service/geocoding/BingGeocodingService.ts
+++ src/main/service/geocoding/BingGeocodingService.ts
@@ -54,18 +54,45 @@ type BingAutocompleteResponse = {
         resources: Array<{
             value: Array<{
                 name: string,
-                address: {
+                address: Partial<{
                     formattedAddress: string;
                     locality: string;
                     adminDistrict: string;
                     postalCode: string;
                     countryRegion: string;
-                };
+                }>;
             }>
         }>;
     }>;
 }
 
+function calculateLevenshteinDistance(a: string, b: string): number {
+    const matrix: number[][] = [];
+
+    for (let i = 0; i <= b.length; i++) {
+        matrix[i] = [i];
+    }
+
+    for (let j = 0; j <= a.length; j++) {
+        matrix[0][j] = j;
+    }
+
+    for (let i = 1; i <= b.length; i++) {
+        for (let j = 1; j <= a.length; j++) {
+            if (b[i - 1] === a[j - 1]) {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            } else {
+                matrix[i][j] = Math.min(
+                    matrix[i - 1][j - 1] + 1,
+                    matrix[i][j - 1] + 1,
+                    matrix[i - 1][j] + 1
+                );
+            }
+        }
+    }
+
+    return matrix[b.length][a.length];
+}
 
 export default class BingGeocodingService implements GeocodingService {
     private readonly axiosInstance = axios.create();
@@ -80,7 +107,7 @@ export default class BingGeocodingService implements GeocodingService {
     }
 
     private formApiGeocodingUrl = (address: string): string => {
-        return `https://dev.virtualearth.net/REST/v1/Locations?q=${address}userRegion=${this.region}&maxResults=1&c=${this.lang}&key=${this.bingKey}`
+        return `https://dev.virtualearth.net/REST/v1/Locations?q=${address}userRegion=${this.region}&maxResults=10&c=${this.lang}&key=${this.bingKey}`
     }
 
     private formApiAutocompleteUrl = (address: string) => {
@@ -94,8 +121,8 @@ export default class BingGeocodingService implements GeocodingService {
     async geocode(address: string): Promise<GeoLocation> {
         const fetchUrl = this.formApiGeocodingUrl(address);
         const response = await this.axiosInstance.get<BingGeocodingResponse>(fetchUrl);
-        console.log(response.data)
-        return response.data
+
+        const locations = response.data
             .resourceSets
             .map(resourceSet => {
                 return resourceSet.resources;
@@ -109,7 +136,23 @@ export default class BingGeocodingService implements GeocodingService {
                     address: resource.address.formattedAddress
                 }
                 return location;
-            })[0]
+            });
+
+        let bestLocation: GeoLocation|null = null;
+
+        let minimalDeviation: number = Number.MAX_VALUE;
+
+        for (const location of locations) {
+            const deviation = calculateLevenshteinDistance(address, location.address);
+            if (deviation < minimalDeviation) {
+                minimalDeviation = deviation;
+                bestLocation = location;
+            }
+        }
+
+        if (!bestLocation) throw new Error("no location found");
+
+        return bestLocation;
     }
 
 
@@ -140,7 +183,10 @@ export default class BingGeocodingService implements GeocodingService {
             .flat()
             .map(res=>res.value)
             .flat()
-            .map(place=>place.address.formattedAddress);
+            .map(place=>{
+                return place.address.formattedAddress;
+            })
+            .filter((a): a is string=>typeof a === 'string');
     }
 
 
