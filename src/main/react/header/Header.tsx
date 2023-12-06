d3d6257f7a5ad4ede5fe9654cc0edb4a852a4bb3
secret <x@gmail.com>
2023-12-07 00:43:59 +0200

    root navlink

--- src/main/react/header/Header.tsx
+++ src/main/react/header/Header.tsx
@@ -6,6 +6,8 @@ import {useAppSelector} from "../../redux/hooks";
 import BackButton from "../sharedComponents/BackButton";
 import {LitmusServiceContext} from "../App";
 import {BackBtnOptions} from "../RootScreen";
+import {NavLink} from "react-router-dom";
+import appConfig from "../../config/appConfig";
 
 type PropsType = {
     backBtnOptions: BackBtnOptions,
@@ -19,7 +21,9 @@ const Header = ({backBtnOptions: {backPath, displayBtn}}: PropsType) => {
         <header className="header">
             <h2 style={{
                 margin: 0
-            }}>LITMUS</h2>
+            }}>
+                <NavLink className={"header-logo-nav"} to={appConfig.applicationMappings.root}>LITMUS</NavLink>
+            </h2>
                 <div className="header__interact">
                     {displayBtn&&<BackButton path={backPath}/>}
                     <div className="header__avatar-container" onClick={e=>{
