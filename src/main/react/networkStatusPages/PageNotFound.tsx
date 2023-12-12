04d00b3fce2be9d7ce38a1dfb89ac804450bf942
secret <x@gmail.com>
2023-12-13 00:56:17 +0200

    redundant block remove

--- src/main/react/networkStatusPages/PageNotFound.tsx
+++ src/main/react/networkStatusPages/PageNotFound.tsx
@@ -8,7 +8,6 @@ export default function PageNotFound () {
         <div className="page-not-found">
             <div className="page-not-found__content-block">
                 <h1 className="page-not-found__status-code">404</h1>
-                <div className="page-not-found__strip"></div>
                 <div className="page-not-found__details-container">
                     <p className="page-not-found__details">Oops... Seems you try to get the page that not exists</p>
                     <NavLink to={appConfig.applicationMappings.root}>Go home</NavLink>
